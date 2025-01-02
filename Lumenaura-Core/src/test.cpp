f72ac3dee681549eac8998dcbb9c4fe6cc012877
trenaud5298 <trenaud_5298@email.ric.edu>
2025-01-02 01:25:43 -0500

    Initial Commit Of Project Setup, Version 2025.0.1

---
+++ Lumenaura-Core/src/test.cpp
@@ -0,0 +1,6 @@
+#include <Lumenaura/test.h>
+#include <iostream>
+
+void LumenauraLibTest() {
+    std::cout<<"Hello World From The Lumenaura Library!"<<std::endl;
+}
---
+++ Lumenaura-Test/src/main.cpp
@@ -0,0 +1,8 @@
+#include <Lumenaura/test.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout<<"This Is The Lumenaura-Test.exe File Running!"<<std::endl;
+    LumenauraLibTest();
+    return 0;
+}
